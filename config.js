b61abbf13ab59a69f7bcd11f59d7102faaa68dc2
Roman Hrytsyshyn <romangry.2004@gmail.com>
2023-09-14 10:47:45 +0200

    Add certificate

--- config.js
+++ config.js
@@ -1,6 +1,9 @@
 const config = {
   PORT: 4201,
   STATIC_DIR: 'website/dist/website',
+  PASSPHRASE: 'httpsangular',
+  CERT_PATH: 'cert.pem',
+  KEY_PATH: 'key.pem',
 };
 
 export default config;
--- server.js
+++ server.js
@@ -1,8 +1,10 @@
+import fs from 'fs';
+import https from 'https';
 import express from 'express';
 import config from './config.js';
 
 const app = express();
-const { PORT, STATIC_DIR } = config;
+const { PORT, STATIC_DIR, PASSPHRASE, CERT_PATH, KEY_PATH } = config;
 
 app.use(express.static(STATIC_DIR));
 
@@ -10,4 +12,13 @@ app.get('/', (req, res) => {
   res.sendFile('index.html');
 });
 
-app.listen(PORT, () => console.log(`Server has been started on port ${PORT}.`));
+https
+  .createServer(
+    {
+      cert: fs.readFileSync(CERT_PATH),
+      key: fs.readFileSync(KEY_PATH),
+      passphrase: PASSPHRASE,
+    },
+    app,
+  )
+  .listen(PORT, () => console.log(`Server has been started on port ${PORT}`));
